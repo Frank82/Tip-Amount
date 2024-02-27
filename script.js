87632f7c563c269f0e137ed99ea8b422659a3014
Frankie <geos_tricks@outlook.com>
2024-02-27 13:25:11 +0000

    Tip amount calculation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ tipPercentage = Number(prompt('tip %?')) / 100;
 tipAmount = food * tipPercentage;
 total = food + tipAmount;
 
+// Shows food costs and tip amount + total
 console.log('Food Amount:', food);
 console.log('Tip Amount:', tipAmount);
 console.log('Total:', total);
